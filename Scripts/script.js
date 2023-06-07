a23099f1d0d78a3bd858eea6578c1ae37c819343
Juelcio Junior <joelsonjunio1@gmail.com>
2023-06-07 19:50:52 +0100

    script

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,7 +2,7 @@ function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('light')
 
-    const img = document.querySelector("#profile")
+    const img = document.querySelector("#profile img")
 
     if(html.classList.contains('light')){
         img.setAttribute('src', 'assets/vicente-dia.jpg')
